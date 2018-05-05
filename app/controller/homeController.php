0dee77bcd9b8c0278572eb03f6dc050c33350897
fiendishTepes <fiendish.tepes@gmail.com>
2018-05-05 22:49:03 +0700

    00

--- app/controller/homeController.php
+++ app/controller/homeController.php
@@ -11,6 +11,7 @@ class homeController
    {
        echo 'I am in <font color=green>'.__CLASS__.'</font> method <font color=red>'.__METHOD__ .'</font>';
    }
+   
    public function contact()
    {
        echo 'I am in <font color=green>'.__CLASS__.'</font> method <font color=red>'.__METHOD__ .'</font>';
